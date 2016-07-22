eb116f0cad0a5faf80c50b738f497b72daa40001
Ryan Walker <ryan.walker@rackspace.com>
2016-07-22 17:03:04 -0500

    Add install_requires

--- setup.py
+++ setup.py
@@ -7,6 +7,23 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['tests']),
     test_suite='tests',
+    install_requires=[
+        'boto3==1.3.0',
+        'botocore==1.4.24',
+        'docutils==0.12',
+        'futures==3.0.5',
+        'Jinja2==2.8',
+        'jmespath==0.9.0',
+        'lambda-uploader==1.0.0',
+        'MarkupSafe==0.23',
+        'python-dateutil==2.5.3',
+        'PyYAML==3.11',
+        'ruamel.ordereddict==0.4.9',
+        'ruamel.yaml==0.11.11',
+        'six==1.10.0',
+        'virtualenv==15.0.2',
+        'yoke==0.0.1',
+    ],
     classifiers=[
         "Programming Language :: Python :: 2.7"
     ],
