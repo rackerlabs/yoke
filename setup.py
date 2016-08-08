ab6aea813370bfbb8e7899a7c2f13d55ddf75c72
Ryan Walker <ryan.walker@rackspace.com>
2016-08-08 13:24:24 -0500

    Cleanup requirements

--- setup.py
+++ setup.py
@@ -10,21 +10,11 @@ setup(
     install_requires=[
         'boto3==1.3.0',
         'botocore>=1.4.24',
-        'docutils==0.12',
-        'futures==3.0.5',
         'Jinja2==2.8',
-        'jmespath==0.9.0',
         'jsonref==0.1',
         'lambda-uploader==1.0.0',
-        'MarkupSafe==0.23',
-        'python-dateutil==2.5.3',
-        'PyYAML==3.11',
         'retrying==1.3.3',
-        'ruamel.ordereddict==0.4.9',
         'ruamel.yaml==0.11.11',
-        'six==1.10.0',
-        'virtualenv==15.0.2',
-        'yoke==0.0.1',
     ],
     classifiers=[
         "Programming Language :: Python :: 2.7"
